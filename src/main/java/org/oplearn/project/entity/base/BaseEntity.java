6edb00ac166419d158506bda362cee57c25ffa4c
taduyhieu2004 <hieu391968@gmail.com>
2024-05-02 16:01:32 +0700

    test

--- src/main/java/org/oplearn/project/entity/base/BaseEntity.java
+++ src/main/java/org/oplearn/project/entity/base/BaseEntity.java
@@ -10,7 +10,6 @@ import org.springframework.data.jpa.domain.support.AuditingEntityListener;
 @EntityListeners(AuditingEntityListener.class)
 public abstract class BaseEntity {
   @Id
-  @GeneratedValue(strategy = GenerationType.IDENTITY)
-  private Integer id;
+  private String id;
 }
 
